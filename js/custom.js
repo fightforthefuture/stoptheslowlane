227ec4c478488be3c01be4fb25000ba9f9dbe38e
Jeff Lyon <jeff@rubbingalcoholic.com>
2014-05-12 08:34:28 -0700

    Get the code is now always an inline link

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 (function(){
-  "use strict";
-	//Banner height
+  	//Banner height
 	$(document).ready(function() {
 
+		// JL NOTE ~ disabled because it was resizing to max height on my 30"
 		/*
 		var viewport_width = window.innerWidth;
 		var viewport_height = window.innerHeight;
@@ -16,36 +16,45 @@
 			$('.banner,.carousel .item').css('min-height', viewport_height);
 		});
 		*/
+
+		var initStream = function(elem, delay) {
+
+			var rset = "01";
+			var width = 250;
+			var iterations = 0;
+			var MAX_ITERATIONS = 30; // should be adjusted to fill 27" cinema
+			var intervalId = setInterval(function() {
+				if (iterations > MAX_ITERATIONS)
+					return clearInterval(intervalId);
+				else				
+					iterations++;
+
+				var buf = [];
+
+				for(var i = 0; i < width; i++)
+					buf[i]=rset[Math.floor(Math.random()*rset.length)]; 
+				
+			  	$("#"+elem).text($("#"+elem).text() + buf.join(''));
+			}, delay);
+		}
+
+		initStream('cascade-one', 50);
+		initStream('cascade-two', 300);
+		initStream('cascade-three', 900);
+
 	});
 
-	// Below lies horrible code that needs to be refactored. Read at your own risk.
-
-	var initStream = function(elem, delay) {
-
-		var rset = "01";
-		var width = 250;
-		var iterations = 0;
-		var MAX_ITERATIONS = 30; // should be adjusted to fill 27" cinema display
-		var intervalId = setInterval(function() {
-			if (iterations > MAX_ITERATIONS)
-				return clearInterval(intervalId);
-			else				
-				iterations++;
-
-			var buf = [];
-
-			for(var i = 0; i < width; i++)
-				buf[i]=rset[Math.floor(Math.random()*rset.length)]; 
-			
-		  	$("#"+elem).text($("#"+elem).text() + buf.join(''));
-		}, delay);
-	}
-
-	initStream('cascade-one', 50);
-	initStream('cascade-two', 300);
-	initStream('cascade-three', 900);
 	
-
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length?target:$('[name='+this.hash.slice(1)+']');
+			if (target.length) {
+				$('html,body').animate({scrollTop: target.offset().top}, 1000);
+				return false;
+			}
+		}
+	});
 
 })();
 
